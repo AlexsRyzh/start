c75382412653eb2ce9f905602d8b12049d77772b
Alexs <alexsandr.ryzhkov03@mail.ru>
2022-12-05 22:44:55 +0300

    ver 1.0.0

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App/App';
 import "./reset.scss"
 
 
-////cfsvs
+////cfsvsscfvsfv
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
